231964a6b73c29cd64478d6e45d65b6f34aa8d0b
ping <super10793@gmail.com>
2023-04-25 16:02:50 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/ubike/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.demo.ubike
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.ubike.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
