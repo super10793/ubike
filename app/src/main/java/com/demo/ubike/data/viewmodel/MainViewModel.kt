d7cf5064edf4d9000c6b497e2246079a65335d7c
ping <super10793@gmail.com>
2023-05-21 13:28:12 +0800

    [MOD]activity的viewmodel使用RxJavaPlugins.setErrorHandler攔截錯誤

--- app/src/main/java/com/demo/ubike/data/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/demo/ubike/data/viewmodel/MainViewModel.kt
@@ -1,7 +1,19 @@
 package com.demo.ubike.data.viewmodel
 
-import androidx.lifecycle.ViewModel
+import io.reactivex.exceptions.CompositeException
+import io.reactivex.plugins.RxJavaPlugins
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel : BaseViewModel() {
+    init {
+        setErrorHandler()
+    }
+
+    private fun setErrorHandler() {
+        RxJavaPlugins.setErrorHandler {
+            if (it !is CompositeException) return@setErrorHandler
+            it.exceptions.forEach { error ->
+                println("setErrorHandler error = $error")
+            }
+        }
+    }
 }
