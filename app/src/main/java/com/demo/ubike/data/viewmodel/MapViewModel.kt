62dd817ae58ea0a8367e2b69533c16f3f235f69b
ping <super10793@gmail.com>
2023-06-12 14:37:19 +0800

    [MOD]調整splash背景 [RM]移除不需要的import

--- app/src/main/java/com/demo/ubike/data/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/demo/ubike/data/viewmodel/MapViewModel.kt
@@ -17,7 +17,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
