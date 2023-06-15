f29a60434ffda8982c5be3c3bf1aa35477cbda9d
ping <super10793@gmail.com>
2023-06-16 02:20:24 +0800

    [MOD]更新敏感資訊並將其儲存到secrets.properties中(舊client_id、client_secret已失效)

--- app/src/main/java/com/demo/ubike/Config.kt
+++ app/src/main/java/com/demo/ubike/Config.kt
@@ -3,8 +3,8 @@ package com.demo.ubike
 object Config {
     const val BASE_URL = "https://tdx.transportdata.tw/"
     const val GRANT_TYPE = "client_credentials"
-    const val CLIENT_ID = "super10793-12949aee-7824-4a0f"
-    const val CLIENT_SECRET = "c462fc92-0ace-4d2f-be45-dfc8873ee72c"
+    const val CLIENT_ID = BuildConfig.API_CLIENT_ID
+    const val CLIENT_SECRET = BuildConfig.API_CLIENT_SECRET
     const val API_FORMAT = "JSON"
     const val API_STATION_URL = "api/basic/v2/Bike/Station/City/%s"
     const val API_STATION_DETAIL_URL = "api/basic/v2/Bike/Availability/City/%s"
